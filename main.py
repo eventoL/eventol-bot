5460f59626a545a6e54aa7e793604225cdea69ca
FedeG <federicogonzalez761@gmail.com>
2019-07-17 15:44:40 -0300

    Add inline keyboard example

--- main.py
+++ main.py
@@ -31,6 +31,25 @@ def aceptar_cachivache(bot, update):
     update.message.reply_sticker(random.choice([IAN, IAN_LEFT]))
     update.message.reply_text(f'A mi me sirve {username} Gracias!')
 
+
+def preguntar_por_cachivache(bot, update):
+    keyboard = [[InlineKeyboardButton("Si", callback_data='1'),
+                 InlineKeyboardButton("No", callback_data='2')],
+                [InlineKeyboardButton("Quizas podria usarlo", callback_data='3')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Alguien mas lo quiere?', reply_markup=reply_markup)
+
+
+def respuesta(bot, update):
+    query = update.callback_query
+    if query.data == '1':
+        query.edit_message_text(text="Uhhhh, bueno. Lo siguiente me lo llevo")
+    if query.data == '2':
+        query.edit_message_text(text="Genial, mañana me lo llevo")
+    if query.data == '3':
+        query.edit_message_text(text="Bueno, no hay problema")
+
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -39,7 +58,9 @@ def error(update, context):
 updater = Updater(TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('quienquiere', aceptar_cachivache))
-updater.dispatcher.add_handler(MessageHandler(None,callback=process_message))
+updater.dispatcher.add_handler(CommandHandler('alguienquiere', preguntar_por_cachivache))
+updater.dispatcher.add_handler(CallbackQueryHandler(respuesta))
+updater.dispatcher.add_handler(MessageHandler(None, callback=process_message))
 updater.dispatcher.add_error_handler(error)
 
 updater.start_polling()
