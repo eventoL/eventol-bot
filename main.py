8c0fa5e8c811d7d4460e1d10a5f80c9d4f1e7a2e
FedeG <federicogonzalez761@gmail.com>
2019-07-17 15:43:23 -0300

    Add logger and add handler for errors

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
 import sys
-from collections import defaultdict, deque
+import logging
 
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from collections import defaultdict, deque
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler
 
 from constants import TRIGGERS
 
+TOKEN = ''
 
-TOKEN = '742576280:AAH5iPOg5WHIgDrt'
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def process_message(bot, update):
-    chat_id = update.message.chat.id
     message_content = update.message.text
     if message_content[0] != ['/']:      
         message_content_lower = message_content.lower()
@@ -22,12 +25,18 @@ def process_message(bot, update):
 
 def aceptar_cachivache(bot, update):
     username = update.message.from_user.first_name
-    update.message.reply_text(f'🙋‍♂️A mi me sirve {username} Gracias!')
+    update.message.reply_text(f'A mi me sirve {username} Gracias!')
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 updater = Updater(TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('quienquiere', aceptar_cachivache))
 updater.dispatcher.add_handler(MessageHandler(None,callback=process_message))
+updater.dispatcher.add_error_handler(error)
 
 updater.start_polling()
 updater.idle()
