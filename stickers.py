4bdf7b976de1115cdeafe13ce253cd753e3e0a4d
FedeG <federicogonzalez761@gmail.com>
2019-07-17 15:42:26 -0300

    Add stickers constants

---
+++ stickers.py
@@ -0,0 +1,4 @@
+from telegram import Sticker
+
+IAN = Sticker('https://gitlab.com/FedeG/ropavejero_bot/raw/master/stickers/ian.webp', width=10, height=10)
+IAN_LEFT = Sticker('https://gitlab.com/FedeG/ropavejero_bot/raw/master/stickers/ian-left.webp', width=10, height=10)
